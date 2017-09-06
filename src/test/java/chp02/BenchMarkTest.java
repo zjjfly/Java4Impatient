3fc05574f0ad58430bae8d5ea6151a3b32a2c18b
zjjfly <zjjblue@gmail.com>
2017-09-06 14:55:39 +0800

    使用注解配置JMH

--- src/test/java/chp02/BenchMarkTest.java
+++ src/test/java/chp02/BenchMarkTest.java
@@ -9,10 +9,15 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-@BenchmarkMode({Mode.SingleShotTime})
+@BenchmarkMode({Mode.AverageTime})
+@Warmup(iterations = 5)
+@Measurement(iterations = 10)
+@Fork(10)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class BenchMarkTest {
     @State(Scope.Benchmark)
     public static class BenchmarkState {
