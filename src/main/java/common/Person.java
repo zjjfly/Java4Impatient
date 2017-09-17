b709579cfa09ef3782ab0a6397b8cb0f045ff3ca
zjjfly <zjjblue@gmail.com>
2017-09-17 14:03:33 +0800

    add lombok support

--- src/main/java/common/Person.java
+++ src/main/java/common/Person.java
@@ -1,35 +1,11 @@
 package common;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Person {
-    private String name;
     private int id;
-
-    public Person(int age, String name) {
-        this.id = age;
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                '}';
-    }
+    private String name;
 }
